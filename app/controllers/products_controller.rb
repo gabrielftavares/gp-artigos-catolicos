c2c4c77e5583e98f8f86ce33161ca241c373d684
gabriel tavares <gabriel29t@gmail.com>
2022-04-15 19:55:03 -0300

    edit and delete method added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,12 @@
 class ProductsController < ApplicationController
+  before_action :set_params, only: %i[show edit update destroy]
   before_action :authenticate_user!, except: %i[index show]
 
   def index
     @products = Product.all
   end
 
-  def show
-    @product = Product.find(params[:id])
-  end
+  def show; end
 
   def new
     @product = Product.new
@@ -22,7 +21,25 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    redirect_to '/' if @product.destroy
+  end
+
   private
+
+  def set_params
+    @product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:name, :description, :category_id, :price, :photo)
